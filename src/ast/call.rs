8ba0029ff697e8ae9e3e4e1bf39c64f401a7d43e
Cyber Gsus <cyber.gsuscode@gmail.com>
2021-09-07 10:52:14 +0200

    Revert "Revert "refactor(all)""
    This reverts commit e710933e216a76072e2ed2cef2d57acbddb89c36.

--- src/ast/call.rs
+++ src/ast/call.rs
@@ -1,4 +1,4 @@
-use super::parser::{Parse, ParseRes, Parser};
+use super::parser::{Parse, ParseErrorKind, ParseRes, Parser};
 use super::HasSpan;
 use super::Lookup;
 use super::Token;
@@ -19,9 +19,123 @@ use std::ops::Range;
 /// integration of Voila to the rest of the system.
 #[derive(Debug)]
 pub struct Call<'source> {
-    function_name: &'source str,
+    function_kind: Function,
     arguments: Vec<Arg<'source>>,
     span: Range<usize>,
+}
+
+/// The function that is [`Call`]ed.
+///
+/// # Panic
+/// The interpreter will panic when the function has not
+/// enough arguments to execute.
+#[derive(Debug)]
+pub enum Function {
+    /// Create a directory with its parents recursively.
+    /// This function is not destructive, it will error
+    /// if a file with the name of the directory exists already.
+    ///
+    /// # Call format
+    /// `mkdir` receives one argument: the path to create
+    Mkdir,
+    /// Print something to standard output
+    ///
+    /// # Call format
+    /// `print` receives a variadic number of arguments which it prints
+    /// separated by spaces (similar to python's print function without parameters), and a newline
+    /// after.
+    Print,
+    /// Execute a command in `sh`
+    ///
+    /// # Call format
+    /// `shell` needs at least one argument. When called, it gets all the arguments, joins all by spaces
+    /// and feeds that to `sh -c` in the case of linux and `powershell` on windows. No input is given to it, so things like `sudo`
+    /// won't work unless you start voila with privileges
+    ///
+    /// # Safety
+    /// This function may modify the outer system!
+    Shell,
+    /// Delete the given file
+    ///
+    /// # Call format
+    /// `delete` receives one argument: the file/directory to delete.
+    /// Directories are deleted recursively!
+    ///
+    /// # Safety
+    /// `delete` will modify the outer system! Make sure that you're not doing
+    /// accesses to the file in the argument on the same cycle, otherwise you will
+    /// get undefined behavior.
+    Delete,
+    /// Moves or renames a file, with a similar behavior to the `mv` command.
+    ///
+    /// # Call format
+    /// `move` receives two arguments: the source file/directory and the target destination
+    ///
+    /// # Safety
+    /// `move` is a destructive call, so please make sure that you're not using it with the same file in the same cycle. Refer to [`Function::Delete`] for details
+    Move,
+    /// Copy a file or a directory. Directories are copied recursively.
+    ///
+    /// # Call format
+    /// `copy` receives two arguments: the source file/directory and the target destination
+    ///
+    /// # Safety
+    /// `copy` might overwrite files in the system, so use it carefully! Avoid using it in the same
+    /// cycle unless you can prove it's safe to do so.
+    Copy,
+    /// Gzip a file or a directory. Directories are gzipped recursively.
+    ///
+    // NOTE: please rename this to `gzip` and `gunzip` like the binutils
+    /// # Call format
+    /// `gzc` receives two arguments: the source file/directory to compress and the file to save it
+    /// into. Note that the destination name is not manipulated in any way (nothing is added or
+    /// removed to it)
+    ///
+    /// # Safety
+    /// Since `gzc` has an output file. it may overwrite another that's in the system.
+    GzipCompress,
+    /// Gunzip a file into a file/directory.
+    ///
+    /// # Call format
+    /// `gzd` receives two arguments: the gzipped file, and the destination to decompress into.
+    /// The destination, if not specified, is the directory in which the gzipped file is, **not the
+    /// one that voila is executing in**
+    ///
+    /// # Safety
+    /// since `gzd` has an output directory, it may overwrite a lot af files! Use with care.
+    GzipDecompress,
+}
+
+impl Function {
+    pub const fn minimum_arg_count(&self) -> u8 {
+        match self {
+            Self::Copy | Self::Move | Self::GzipCompress | Self::GzipDecompress => 2,
+            Self::Delete | Self::Shell | Self::Mkdir => 1,
+            Self::Print => 0,
+        }
+    }
+    fn from_name(source: &str) -> Option<Self> {
+        Some(match source {
+            "copy" => Self::Copy,
+            "move" => Self::Move,
+            "gzc" => Self::GzipCompress,
+            "gzd" => Self::GzipDecompress,
+            "delete" => Self::Delete,
+            "shell" => Self::Shell,
+            "mkdir" => Self::Mkdir,
+            "print" => Self::Print,
+            _ => return None,
+        })
+    }
+}
+
+impl Parse<'_> for Function {
+    fn parse(parser: &mut Parser) -> ParseRes<Self> {
+        // this doesn't accept the token because it is accepted by the calling parser (`Call`), so
+        // it can use the identifier start as a more accurrate start of the function span.
+        let src = parser.expect_token(Token::Identifier, Some("as the name of the function"))?;
+        Self::from_name(src).ok_or_else(|| parser.error(ParseErrorKind::UnknownFunction))
+    }
 }
 
 impl HasSpan for Call<'_> {
@@ -140,8 +254,7 @@ impl<'source> Arg<'source> {
 impl<'source> Parse<'source> for Call<'source> {
     fn parse(parser: &mut Parser<'source>) -> ParseRes<Self> {
         parser.with_context("parsing function call", |parser| {
-            let function_name =
-                parser.expect_token(Token::Identifier, Some("name for the function to call"))?;
+            let function_kind = parser.parse()?;
             let start = parser.current_token_span().start;
             parser.accept_current();
             parser.expect_token(
@@ -215,7 +328,7 @@ impl<'source> Parse<'source> for Call<'source> {
             parser.accept_current();
 
             Ok(Self {
-                function_name,
+                function_kind,
                 arguments,
                 span: start..end,
             })
