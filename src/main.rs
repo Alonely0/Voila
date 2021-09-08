cdf2dc47d95710f6ab4fa7ed15c2750065430865
guillem <4lon3ly0@tutanota.com>
2021-09-08 14:11:57 +0200

    rewrite header and tail messages on debug as raw strings

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,17 @@ fn main() {
     let cli_args: cli::Cli = cli::get_cli_args();
 
     println_on_debug!(
-        "\nVoila v{ver}, DEBUG ENABLED\n
+        r#"
+Voila v{ver}, DEBUG ENABLED
+
 Debug is always enabled on development versions.
 Development versions aren't stable at all, use them at your own risk.
 On non-development versions, enable debug only if you are going to report an error,
 the flood of debug logs may hide warnings. Debug messages may contain sensitive information,
 like the name of the files and other data related to variables.
-For more information see the README.\n
-------------------------------  VOILA EXECUTION STARTED  ------------------------------",
+For more information see the README.
+
+------------------------------  VOILA EXECUTION STARTED  ------------------------------"#,
         ver = env!("CARGO_PKG_VERSION")
     );
 
@@ -32,6 +35,7 @@ For more information see the README.\n
     block_on(interpreter::run(ast, cli_args.dir, cli_args.recursive)); // wait interpreter to finish
 
     println_on_debug!(
-        "\n------------------------------  VOILA EXECUTION ENDED  --------------------------------"
+        r#"
+------------------------------  VOILA EXECUTION ENDED  --------------------------------"#
     )
 }
